7f29561c470fac268b291cdf8a5cb36eaf734c67
Gilbert Omosa <gilbert.omosa@student.moringaschool.com>
2023-10-12 17:33:08 +0300

    write function to fetch data from server

--- script.js
+++ script.js
@@ -4,3 +4,24 @@ document.addEventListener("DOMContentLoaded", function () {
     const memberDetails = document.getElementById("member-details");
 
     let data = []; // Initialize an empty array to store member data
+
+    // Fetch data from your local server and populate the list
+    function fetchAndPopulateList() {
+
+        // Fetch member data from the server
+        fetch("http://localhost:3000/members")
+            .then((response) => response.json())
+            .then((fetchedData) => {
+
+                data = fetchedData;
+
+                populateList(data);
+
+                if (data.length > 0) {
+                    displayMemberDetails(data[0]);
+                }
+            })
+            .catch((error) => {
+                console.error("Error fetching data:", error);
+            });
+    }
