92e7c48652cf36085285963323052b78f509c420
Gilbert Omosa <gilbert.omosa@student.moringaschool.com>
2023-10-12 17:31:52 +0300

    write DOMContentLoaded event listener

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const listGroup = document.querySelector(".list-group");
+    const memberDetails = document.getElementById("member-details");
+
+    let data = []; // Initialize an empty array to store member data
